6f996023b10280fd60334ee3814ff137f7790c28
Premjeet Sahu <cs2054@global.org.in>
2024-04-13 14:42:10 +0530

    default user

--- src/features/authentication/LoginForm.jsx
+++ src/features/authentication/LoginForm.jsx
@@ -7,8 +7,8 @@ import { useLogin } from "./useLogin";
 import SpinnerMini from "../../ui/SpinnerMini";
 
 function LoginForm() {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+  const [email, setEmail] = useState("premjeetsahu254@gmail.com");
+  const [password, setPassword] = useState("12345");
   const { login, isLoading } = useLogin();
 
   function handleSubmit(e) {
